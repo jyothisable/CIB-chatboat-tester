017a33257ef44f0da42ff8d534f400a0299f681c
athul jyothis <athuljyothis18@gmail.com>
2024-02-15 13:35:54 +0530

    Refactoring : making code modular

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ from selenium.webdriver.common.action_chains import ActionChains
 import os.path
 from datetime import datetime
 
+# Constants
+LIMIT = 20 # number of responses until which random activity will be done to keep the session active
+URL ='https://cibnext.icicibank.com/corp/AuthenticationController?FORMSGROUP_ID__=AuthenticationFG&__START_TRAN_FLAG__=Y&FG_BUTTONS__=LOAD&ACTION.LOAD=Y&AuthenticationFG.LOGIN_FLAG=1&BANK_ID=ICI'
 
+# Import driver
 driver = webdriver.Chrome()
 
 # Open the login page
-driver.get('https://cibnext.icicibank.com/corp/AuthenticationController?FORMSGROUP_ID__=AuthenticationFG&__START_TRAN_FLAG__=Y&FG_BUTTONS__=LOAD&ACTION.LOAD=Y&AuthenticationFG.LOGIN_FLAG=1&BANK_ID=ICI')
-
+driver.get(URL)
 # Wait for the page to load completely
 time.sleep(2)
 
@@ -24,18 +27,24 @@ with open('credentials.txt') as f:
     username = lines[0]
     password = lines[1]
 
+def login(username, password):
+    '''
+    Complete login and go to home page
+    '''
+    # Find the username field and enter the username
+    username_field = driver.find_element(By.XPATH,'/html/body/form[1]/div[3]/main/div/div[1]/div[1]/div/div[2]/div[1]/div/div/input')
+    username_field.clear()
+    username_field.send_keys(username)
 
-# Find the username field and enter the username
-username_field = driver.find_element(By.XPATH,'/html/body/form[1]/div[3]/main/div/div[1]/div[1]/div/div[2]/div[1]/div/div/input')
-username_field.clear()
-username_field.send_keys(username)
+
+    # Find the password field and enter the password
+    password_field = driver.find_element(By.XPATH,'//*[@id="AuthenticationFG.ACCESS_CODE"]')
+    password_field.send_keys(password)
+
+    time.sleep(3)
 
 
-# Find the password field and enter the password
-password_field = driver.find_element(By.XPATH,'//*[@id="AuthenticationFG.ACCESS_CODE"]')
-password_field.send_keys(password)
-
-time.sleep(3)
+login(username, password)
 
 # find submit button
 driver.find_element(By.XPATH,'//*[@id="LoginHDisplay"]/main/div/div[1]/div[1]/div/div[2]/div[8]').click()
@@ -66,16 +75,39 @@ def get_response(prompt):
         res_string += str(responses[idx].text) + '#'
     return res_string
 
+def do_some_random_activity():
+    '''
+    Minimize the chatbot then move the cursor down and up to keep the session active then open the chatbox again
+    '''
+    # click on minimize
+    shadow_root.find_element(By.CSS_SELECTOR, '#minimize').click()
+    
+    # Other activity click
+    # Create an ActionChains object
+    actions = ActionChains(driver)
+
+    # Scroll down and up by  100 pixels
+    actions.move_to_element(driver.find_element(By.XPATH,'//*[@id="GroupletPanel.SubSection2"]/div[3]/div[1]'))
+    actions.click()
+    actions.send_keys(Keys.PAGE_DOWN)
+    actions.send_keys(Keys.PAGE_UP)
+
+    # Perform the action
+    actions.perform()
+    
+    # click on chat button 
+    shadow_root.find_element(By.CSS_SELECTOR,'div.wrapper-container > button').click()
+    time.sleep(2)
+
 # get the list of prompts from csv
 df = pd.read_csv('prompt.csv')
 
 df['Response'] = None
 
 # Split the prompts to limit columns such that response can be saved to csv file in between
-limit = 2
 for i in range(len(df)):
     df['Response'].iloc[i] = get_response(df['Prompts'].iloc[i])
-    if i%limit == 0:
+    if i%LIMIT == 0:
         df_limit =df['Response'].str.split(pat='#',regex=False , expand=True)
 
         # timestamped log file
@@ -87,35 +119,15 @@ for i in range(len(df)):
 
         print(f"Data saved to {filename}")
         
-        # do activity to extend the session timeout
-        # click on minimize
-        shadow_root.find_element(By.CSS_SELECTOR, '#minimize').click()
+        do_some_random_activity()
         
-        # Other activity click
-        # Create an ActionChains object
-        actions = ActionChains(driver)
+        
 
-        # Scroll down by  100 pixels
-        actions.move_to_element(driver.find_element(By.XPATH,'//*[@id="GroupletPanel.SubSection2"]/div[3]/div[1]'))
-        actions.click()
-        actions.send_keys(Keys.PAGE_DOWN)
-        actions.send_keys(Keys.PAGE_UP)
-
-        # Perform the action
-        actions.perform()
-        
-        # click on chat button 
-        shadow_root.find_element(By.CSS_SELECTOR,'div.wrapper-container > button').click()
-        time.sleep(2)
-        
-while os.path.exists(filename):
-    # Append a timestamp to the filename
-    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-    filename = f'my_data_{timestamp}.csv'
 
 # Final actual save
 df_final = pd.concat([df['Prompts'],df['Response'].str.split(pat='#',regex=False , expand=True)], axis=1)
 df_final.to_csv('reverse.csv', index=False,mode='w')
+Print('Final data saved to reverse.csv')
 
 
 
