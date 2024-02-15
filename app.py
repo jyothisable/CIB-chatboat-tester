991ae290bf06db54ed1b4207ec702425d10cd1f1
athul jyothis <athuljyothis18@gmail.com>
2024-02-15 14:05:17 +0530

    1st level refactoring done

--- app.py
+++ app.py
@@ -42,22 +42,34 @@ def login(username, password):
     password_field.send_keys(password)
 
     time.sleep(3)
+    
+    # find submit button
+    driver.find_element(By.XPATH,'//*[@id="LoginHDisplay"]/main/div/div[1]/div[1]/div/div[2]/div[8]').click()
+    time.sleep(5)
+    
+    print('Login Successful')
 
 
 login(username, password)
 
-# find submit button
-driver.find_element(By.XPATH,'//*[@id="LoginHDisplay"]/main/div/div[1]/div[1]/div/div[2]/div[8]').click()
+def initialize_chatbot():
+    '''
+    Initiate chatbot and give the shadow root (like 2nd fake root for only this widget)
+    '''
+    # Find chatbot button
+    shadow_root = driver.find_element(By.CSS_SELECTOR, '#shadow').shadow_root
 
-time.sleep(5)
-
-# Find chatbot button
-shadow_root = driver.find_element(By.CSS_SELECTOR, '#shadow').shadow_root
-
-shadow_root.find_element(By.CSS_SELECTOR,'div.wrapper-container > button').click()
-time.sleep(2)
+    shadow_root.find_element(By.CSS_SELECTOR,'div.wrapper-container > button').click()
+    time.sleep(2)
+    print('Chatbot initialized successfully')
+    return shadow_root
+    
+shadow_root = initialize_chatbot()
 
 def get_response(prompt):
+    '''
+    For a given prompt gives all the response with '#' as seperator
+    '''
     # Find input prompt field
     input_field = shadow_root.find_element(By.CSS_SELECTOR, 'div.chat-window > div.chat-footer > input')
     input_field.send_keys(prompt)
@@ -98,36 +110,39 @@ def do_some_random_activity():
     # click on chat button 
     shadow_root.find_element(By.CSS_SELECTOR,'div.wrapper-container > button').click()
     time.sleep(2)
+    
+    print('Session timeout extended successfully')
 
 # get the list of prompts from csv
 df = pd.read_csv('prompt.csv')
 
-df['Response'] = None
+def batch_prompt():
+    print('Initiating Prompt Engine')
+    
+    df['Response'] = None
+    # Split the prompts to limit columns such that response can be saved to csv file in between
+    for i in range(len(df)):
+        df['Response'].iloc[i] = get_response(df['Prompts'].iloc[i])
+        if i%LIMIT == 0:
+            df_limit =pd.concat([df['Prompts'],df['Response'].str.split(pat='#',regex=False , expand=True)], axis=1)
 
-# Split the prompts to limit columns such that response can be saved to csv file in between
-for i in range(len(df)):
-    df['Response'].iloc[i] = get_response(df['Prompts'].iloc[i])
-    if i%LIMIT == 0:
-        df_limit =df['Response'].str.split(pat='#',regex=False , expand=True)
+            # timestamped log file
+            timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+            filename = f'logs/reverse_{timestamp}.csv'
 
-        # timestamped log file
-        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-        filename = f'logs/reverse_{timestamp}.csv'
+            # Save the DataFrame to the CSV file
+            df_limit.iloc[:i+1].to_csv(filename, index=False)
 
-        # Save the DataFrame to the CSV file
-        df_limit.iloc[:i+1].to_csv(filename, index=False)
+            print(f"{i%LIMIT+1}th batch completed. Data saved to {filename}")
+            
+            do_some_random_activity()
 
-        print(f"Data saved to {filename}")
-        
-        do_some_random_activity()
-        
-        
-
+batch_prompt()
 
 # Final actual save
 df_final = pd.concat([df['Prompts'],df['Response'].str.split(pat='#',regex=False , expand=True)], axis=1)
 df_final.to_csv('reverse.csv', index=False,mode='w')
-Print('Final data saved to reverse.csv')
+print('Final data saved to reverse.csv')
 
 
 
